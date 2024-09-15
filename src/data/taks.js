fdcd31c66b6ad2813f679c32e325a500eacf48b1
Deck <cristianjbc99@gmail.com>
2024-09-14 19:16:48 -0600

    firts commit

---
+++ src/data/taks.js
@@ -0,0 +1,19 @@
+export const tasks = [
+    {
+        id: 0,
+        title: 'Mi primer tarea',
+        description: 'descripcion 1'
+    },
+    {
+        id: 1,
+        title: 'Mi segunda tarea',
+        description: 'descripcion 2'
+    },
+    {
+        id: 2,
+        title: 'Mi tercer tarea',
+        description: 'descripcion 3'
+    },
+    
+]
+
